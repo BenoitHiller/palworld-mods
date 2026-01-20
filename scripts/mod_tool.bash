9e78ff9c746273b4a5c6ab721a6e0418fac7158d
Benoit Hiller <benoit.hiller@gmail.com>
2026-01-19 21:52:08 -0500

    Add some documentation to the mod_tool

--- scripts/mod_tool.bash
+++ scripts/mod_tool.bash
@@ -31,6 +31,9 @@ clean_dir() {
   rm -r "$@" 2>/dev/null || true
 }
 
+# Build the list of files and folders that need to be copied over into the Steam Workshop folder
+#
+# 1.info_file the path to the Info.json file for the mod
 target_files() {
   local -r info_file="$1"
 
@@ -43,6 +46,9 @@ target_files() {
   echo "Info.json"
 }
 
+# Copy all of the files identified by target_files() into a folder within the OUT_DIR based on the id of this mod
+#
+# Pulls all arguments from the variables configured by load_config()
 package_workshop() {
   local MOD_OUT_DIR="$OUT_DIR/workshop/$WORKSHOP_ID"
   local thumbnail="$(jq -r '.["Thumbnail"]' "$INFO_FILE")"
@@ -53,6 +59,11 @@ package_workshop() {
   rsync -arvh --delete --files-from=<(target_files "$INFO_FILE") "$MOD_FOLDER/" "$MOD_OUT_DIR/"
 }
 
+# Create a vortex compatible zip archive of this mod
+#
+# Note that this currently only places PalSchema and Pak files
+#
+# Pulls all arguments from the variables configured by load_config()
 package_nexus() {
   local MOD_BUILD_DIR="$BUILD_DIR/nexus/$MOD_NAME"
   local MOD_STEAM_FOLDER="$OUT_DIR/workshop/$WORKSHOP_ID"
@@ -86,6 +97,9 @@ package_nexus() {
   )
 }
 
+# Copy the folder produced by package_workshop() into the workshop folder where Steam can find them
+#
+# @. The mods you want to copy as individual arguments
 install_workshop() {
   if [[ -z "$DESTDIR" ]]; then
     error "DESTDIR was not set, unable to perform install"
@@ -104,12 +118,28 @@ install_workshop() {
   done
 }
 
+# Configure the global variables for things that are the same for all mods
+#
+# WORK_DIR: expected to be the root of the git repository. Will take its value from ENV if you specify it there.
+# OUT_DIR: where to place the final folders (not the install location. Will take its value from ENV if you specify it there.
+# BUILD_DIR: where to place intermediary files.
 initialize() {
   WORK_DIR="${WORK_DIR-$PWD}"
   OUT_DIR="${OUT_DIR-$WORK_DIR/out}"
   BUILD_DIR="$WORK_DIR/build"
 }
 
+# Populate global variables used by individual mods
+#
+# 1.MOD_NAME the mod to load configuration for
+#
+# Configured Variables:
+#
+# MOD_FOLDER: where the source for this mod is located
+# INFO_FILE: the Info.json file for the mod. Contains most metadata.
+# WORKSHOP_FILE: the .workshop.json file, which is more like a cache maintained by the uploader. Only really has the workshop id in it.
+# WORKSHOP_ID: the id of the workshop mod on Steam.
+# WORKSHOP_OUT_DIR: where to place the final folder for this mod.
 load_config() {
   MOD_NAME="$1"
 
@@ -121,6 +151,14 @@ load_config() {
   WORKSHOP_OUT_DIR="$OUT_DIR/workshop/$WORKSHOP_ID"
 }
 
+# This is a tool to help with gathering the files for a mod and packaging them
+# as needed for the different platforms.
+#
+# What folders go where is defined within a json file so was just a bit to
+# frustrating to get make to handle directly, at least at the start. I will
+# hopefully be able to roll more of this into the Makefile directly in the
+# future so that I can take advantage of it checking whether it needs to
+# rebuild things.
 main() {
   local -r command_name="$1"
 
